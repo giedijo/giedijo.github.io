243a93748bf0e0ab96a78e1f5a49dd6fde833aa7
giedijo <giedijo.com@gmail.com>
2017-03-06 23:02:56 -0300

    tudo arrumado
    tudo arrumado

--- js/script.js
+++ js/script.js
@@ -119,7 +119,7 @@
 
 		
 		/*CountDown*/
-		$('.married_coundown').countdown({until: new Date(2017, 5, 15)});
+		$('.married_coundown').countdown({until: new Date(2017,3,16)});
 		
 		/*OWL Carousel in Our Story*/
 		$(".story_wrapper").owlCarousel({
